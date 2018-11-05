6e947a9fdd1969d6471192ac4f4b9074e8fa686d
Prod <prodigion@hotmail.com>
2018-11-05 13:56:41 -0500

    Cleanroles command to delete unnecessary meowth roles

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -31,6 +31,32 @@ class UtilityCog:
 
         await ctx.message.delete()
 
+    @commands.command(name='cleanroles')
+    @commands.has_role('Mods')
+    @commands.guild_only()
+    async def clean_roles(self, ctx):
+        """Clean Roles"""
+
+        forbiddenRoles = ['pichu', 'togepi', 'elekid', 'magby', 'smoochum', 'tyrogue', 'azurill', 'wynaut', 'riolu', 'mime jr', 'munchlax',
+                          'corsola', "farfetch'd", 'heracross', 'kangaskhan', 'mr mime', 'relicanth', 'solrock', 'torkoal', 'tropius', 'volbeat', 'zangoose', 'pachirisu', 'chatot', 'carnivine',
+                          'celebi', 'jirachi', 'mew',
+                          'delibird', 'espeon', 'flygon', 'gardevoir', 'kingdra', 'ludicolo', 'metagross', 'milotic', 'porygon2', 'salamence', 'scizor', 'slaking', 'steelix', 'sunflora', 'umbreon', 'wailord',
+                          'gorebyss', 'huntail', 'kecleon', 'smeargle',
+                          'budew', 'roserade', 'cranidos', 'rampardos', 'shieldon', 'bastiodon', 'burmy', 'wormadam', 'mothim', 'combee', 'vespiquen', 'buizel', 'floatzel', 'cherubi', 'cherrim', 'shellos', 'gastrodon', 'ambipom', 'mismagius', 'honchkrow', 'glameow', 'purugly', 'chingling', 'bronzor', 'bronzong', 'bonsly', 'happiny', 'spiritomb', 'gible', 'gabite', 'garchomp', 'hippopotas', 'hippowdon', 'skorupi', 'drapion', 'croagunk', 'toxicroak', 'finneon', 'lumineon', 'mantyke', 'snover', 'abomasnow', 'weavile', 'magnezone', 'lickilicky', 'rhyperior', 'tangrowth', 'electivire', 'magmortar', 'togekiss', 'yanmega', 'leafeon', 'glaceon', 'gliscor', 'mamoswine', 'porygon z', 'gallade', 'probopass', 'dusknoir', 'froslass', 'rotom', 'uxie', 'mesprit', 'azelf', 'dialga', 'palkia', 'heatran', 'regigigas', 'cresselia', 'phione', 'manaphy', 'darkrai', 'shaymin', 'arceus'
+                         ]
+        clearedRoles = 0
+        for delRole in forbiddenRoles:
+          role = discord.utils.get(ctx.guild.roles, name=delRole)
+          if role:
+            try:
+              await role.delete()
+              await ctx.send(f"The role {role.name} has been deleted!")
+              clearedRoles += 1
+            except discord.Forbidden:
+              await ctx.send("Missing Permissions to delete this role!")
+
+        await ctx.send(f"{clearedRoles} Roles cleared.")
+
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case SimpleCog.
 # When we load the cog, we use the name of the file.
 def setup(bot):
