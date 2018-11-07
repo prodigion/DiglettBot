2bcb22c49db09701709818daae18af7d5929d67e
Prod <prodigion@hotmail.com>
2018-11-07 15:10:33 -0500

    Move research query to its own function Add !dugtrio to fetch all available research Simplify !dig so it only accepts mon names

--- cogs/research.py
+++ cogs/research.py
@@ -1,44 +1,69 @@
 import discord
 from discord.ext import commands
+from pymysql.err import OperationalError
+import datetime
 
 class ResearchCog:
     def __init__(self, bot):
         self.bot = bot
 
+    async def get_quest(self, ctx, cur, mon: int):
+        """List all encounters for a specific Pokemon."""
+
+        await cur.execute(f"select * from pokestop where quest_pokemon_id={mon};")
+        numResults = cur.rowcount
+        if numResults == 0:
+            return
+        ctr = 0
+        questList = f"Research for {datetime.date.today():%B %d} - {self.bot.pokedex[f'{mon:03}']}\n\n"
+        print(f'{mon:03}')
+        async for r in cur:
+            ctr += 1
+            questList += f'({ctr}/{numResults}) Poketop: [{r[3]}](http://www.google.com/maps/place/{r[1]},{r[2]})\n'
+            if len(questList) > 1700 or ctr == numResults:
+                await ctx.send(embed=discord.Embed(description=questList))
+                questList = f"Research for {datetime.date.today():%B %d} - {self.bot.pokedex[f'{mon:03}']}\n\n"
+
     @commands.command(name='dig')
     @commands.guild_only()
-    async def get_quests(self, ctx, mon):
-        """List all encounters for a specific Pokemon. Can either be searched by Pokemon name or number."""
+    async def get_research(self, ctx, monName = ""):
+        """List all encounters for a specific Pokemon."""
 
-        try:
-            monName = mon
-            mon = next(key for key, value in self.bot.pokedex.items() if value.lower() == mon.lower())
-        except StopIteration:
-            pass
+        if ctx.channel.name == "pokestop-reports":
+            if monName == "":
+                await ctx.send("Please select a mon to search for. For example, `!dig Chansey`")
+                return
+            try:
+                mon = next(key for key, value in self.bot.pokedex.items() if value.lower() == monName.lower())
+                monName = self.bot.pokedex[mon]
+            except StopIteration:
+                mon = 0
 
-        try:
-            monName = self.bot.pokedex[mon]
-        except KeyError:
-            mon = 0
+            try:
+                async with self.bot.pool.acquire() as conn:
+                    async with conn.cursor() as cur:
+                        await self.get_quest(ctx, cur, int(mon))
+            except OperationalError:
+                await ctx.send('Server is not currently available. Please try again later or use Meowth reporting.')
+
+    @commands.command(name='dugtrio')
+    @commands.guild_only()
+    @commands.has_role('Admins')
+    async def get_all_research(self, ctx):
+        """List all encounters, sorted by Pokemon number"""
 
         if ctx.channel.name == "pokestop-reports":
             async with self.bot.pool.acquire() as conn:
-                try:
-                    async with conn.cursor() as cur:
-                        await cur.execute(f"select * from pokestop where quest_pokemon_id={mon};")
-                        numResults = cur.rowcount
-                        if numResults == 0:
-                            await ctx.send(f"No results found for {monName}")
-                        ctr = 0
-                        questList = f"Today's Research - {monName}\n\n"
-                        async for r in cur:
-                            ctr += 1
-                            questList += f'({ctr}/{numResults}) Poketop: [{r[3]}](http://www.google.com/maps/place/{r[1]},{r[2]})\n'
-                            if len(questList) > 1500 or ctr == numResults:
-                                await ctx.send(embed=discord.Embed(description=questList))
-                                questList = f"Today's Research - {monName}\n\n"
-                except pymysql.err.OperationalError:
-                    await ctx.send('Server is not currently available. Please try again later.')
+                async with conn.cursor() as cur:
+                    await cur.execute(f"select distinct quest_pokemon_id from pokestop;")
+                    numResults = cur.rowcount
+                    if numResults == 0:
+                        await ctx.send(f"No encounters found")
+                    async with self.bot.pool.acquire() as conn2:
+                        async with conn.cursor() as cur2:
+                            async for r in cur:
+                                if r[0]:
+                                    await self.get_quest(ctx, cur2, r[0])
 
 def setup(bot):
     bot.add_cog(ResearchCog(bot))
