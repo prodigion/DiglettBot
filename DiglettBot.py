5c94ff01d2a422a3af8360797e47a3b57cdb2467
Prod <prodigion@hotmail.com>
2018-11-29 14:17:35 -0500

    Remove irrelevant comments

--- DiglettBot.py
+++ DiglettBot.py
@@ -3,28 +3,9 @@ from discord.ext import commands
 import sys, traceback
 import json
 
-"""This is a multi file example showcasing many features of the command extension and the use of cogs.
-These are examples only and are not intended to be used as a fully functioning bot. Rather they should give you a basic
-understanding and platform for creating your own bot.
-
-These examples make use of Python 3.6.2 and the rewrite version on the lib.
-
-For examples on cogs for the async version:
-https://gist.github.com/leovoel/46cd89ed6a8f41fd09c5
-
-Rewrite Documentation:
-http://discordpy.readthedocs.io/en/rewrite/api.html
-
-Rewrite Commands Documentation:
-http://discordpy.readthedocs.io/en/rewrite/ext/commands/api.html
-
-Familiarising yourself with the documentation will greatly help you in creating your bot and using cogs.
-"""
-
 def get_prefix(bot, message):
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
 
-    # Notice how you can use spaces in prefixes. Try to keep them simple though.
     prefixes = ['!']
 
     # Check to see if we are outside of a guild. e.g DM's etc.
@@ -35,8 +16,6 @@ def get_prefix(bot, message):
     # If we are in a guild, we allow for the user to mention us or use any of the prefixes in our list.
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
-# Below cogs represents our folder our cogs are in. Following is the file name. So 'meme.py' in cogs, would be cogs.meme
-# Think of it like a dot path import
 initial_extensions = ['cogs.events',
                       'cogs.members',
                       'cogs.research',
--- cogs/utilities.py
+++ cogs/utilities.py
@@ -66,7 +66,5 @@ class UtilityCog:
 
         await ctx.send(f"{clearedRoles} Roles cleared.")
 
-# The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case SimpleCog.
-# When we load the cog, we use the name of the file.
 def setup(bot):
     bot.add_cog(UtilityCog(bot))
